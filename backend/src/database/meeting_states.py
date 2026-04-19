f138be56f8e80bca943afc38f430417db6970baa
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2026-04-19 06:46:57 +0200

    feat: add WAITING_FOR_CONFIRMATION status to MeetingStatus enum

--- backend/src/database/meeting_states.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class MeetingStatus(str, Enum):
-    COLLECTING_AVAILABILITY = "collecting_availability"
-    READY_FOR_AI = "ready_for_ai"
-    AI_RECOMMENDED = "ai_recommended"
-    FINALIZED = "finalized"
